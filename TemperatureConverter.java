622083d6a7a437eaac3d20ff6d325c025e9e51f6
riya9927 <114976968+riya9927@users.noreply.github.com>
2024-11-12 00:10:02 +0530

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -10,15 +10,18 @@ public class TemperatureConverter {
         System.out.print("Enter the unit (C for Celsius, F for Fahrenheit): ");
         String unit = scanner.next();
 
-        double convertedTemperature;
-        String convertedUnit;
+        double convertedTemperature = 0;
+        String convertedUnit = "";
 
         if (unit.equalsIgnoreCase("C")) {
             convertedTemperature = (temperature * 9 / 5) + 32;
             convertedUnit = "Fahrenheit";
-        } else {
+        } else if (unit.equalsIgnoreCase("F")) {
             convertedTemperature = (temperature - 32) * 5 / 9;
             convertedUnit = "Celsius";
+        } else {
+            System.out.println("Enter valid unit!!!");
+            return; 
         }
 
         System.out.println("The temperature in " + convertedUnit + " is: " + convertedTemperature);
